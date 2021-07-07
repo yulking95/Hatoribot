203a949f7361a4ae0c075efb10cbe8983474d545
yulking95 <83798245+yulking95@users.noreply.github.com>
2021-07-06 23:15:21 -0500

    Create cekvip.js

---
+++ src/cekvip.js
@@ -0,0 +1,25 @@
+const cekvip = () => { 
+
+	return `           ──────────────────
+
+*N. del bot* : HH BOT
+
+──────────────────
+
+   『 *𝐕𝐈𝐏 𝐔𝐒𝐄𝐑* 』
+
+──────────────────
+
+• *Status* : *ACTIVO*
+
+────────────────── 
+
+*Status Bot:* *Online*
+
+──────────────────
+
+*USTED ES UN MIEMBRO PREMIUM* 🐊🚩`
+
+}
+
+exports.cekvip = cekvip
