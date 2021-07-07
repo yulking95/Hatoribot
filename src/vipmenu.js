1c54a521f0e1519cb34e70aaa72058711e6af11c
yulking95 <83798245+yulking95@users.noreply.github.com>
2021-07-07 14:23:46 -0500

    Create vipmenu.js

---
+++ src/vipmenu.js
@@ -0,0 +1,24 @@
+const vipmenu = (prefix) => { 
+	return `            
+	
+╔══✪〘 USUÁRIO VIP 〙✪══
+║
+╠➸ *${prefix}dado*
+╠➸ *${prefix}cekvip*
+╠➸ *${prefix}premiumlist*
+╠➸ *${prefix}delete*
+╠➸ *${prefix}modapk*
+╠➸ *${prefix}daftarvip [para pasar a Premium]*
+╠➸ *${prefix}qrcode*
+╠➥ *${prefix}chentai*
+╠➥ *${prefix}gcpf*
+╠➥ *${prefix}gbin*
+╠➥ *${prefix}pack*
+╠➥ *${prefix}destrava*
+╠➥ *${prefix}gpersona*
+║
+║
+╚══✪〘  HH BOT 〙✪══
+`
+}
+exports.vipmenu = vipmenu
