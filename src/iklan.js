da3070cfb090e965f047f5fe7033e192fa6c4b6b
yulking95 <83798245+yulking95@users.noreply.github.com>
2021-07-07 13:39:01 -0500

    Create iklan.js

---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALQUILER Y CREAR BOTS:*
+╠➥ *ALQUILER: 10 / GRUPO (MÊS)*
+╠➥ *CREAR: 35 (PUEDE SER PROPRIETÁRIO)*
+╠➥ *PUEDE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO, BOLETO,*
+╠═══════════════════════════
+╠➥ *CONTACTO*
+╠➥ *wa.me/51928564603*
+║
+╚═〘  CAUSS  〙
+`
+}
+exports.iklan = iklan
