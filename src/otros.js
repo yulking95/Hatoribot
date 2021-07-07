9eead91c3e8d915fcb68334ac78ac358cf952fbe
yulking95 <83798245+yulking95@users.noreply.github.com>
2021-07-07 14:20:16 -0500

    Create otros.js

---
+++ src/otros.js
@@ -0,0 +1,38 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 *OTROS* 〙✪══
+║
+╠➥ *${prefix}linkgp*
+╠➥ *${prefix}simih [1/0]*
+╠➥ *${prefix}marcar*
+╠➥ *${prefix}add [@]*
+╠➥ *${prefix}banear [@]*
+╠➥ *${prefix}promover [@]*
+╠➥ *${prefix}rebajar*
+╠➥ *${prefix}admins*
+╠➥ *${prefix}marcar2*
+╠➥ *${prefix}marcar3*
+╠➥ *${prefix}bloqueados*
+╠➥ *${prefix}bloquear [@]*
+╠➥ *${prefix}desbloquear [@]*
+╠➥ *${prefix}limpiar*
+╠➥ *${prefix}bienvenido [1/0]*
+╠➥ *${prefix}clonar [@]*
+╠➥ *${prefix}help1*
+╠➥ *${prefix}dueño*
+╠➥ *${prefix}owner*
+╠➥ *${prefix}tts [texto]*
+╠➥ *${prefix}setnombre*
+╠➥ *${prefix}termux*
+╠➥ *${prefix}setfoto*
+╠➥ *${prefix}ytmp4*
+╠➥ *${prefix}buendia*
+╠➥ *${prefix}buenasnoches*
+╠➸ *${prefix}marcar*
+╠➸ *${prefix}marcar2*
+╠➸ *${prefix}marcar3*
+║
+╚═〘  *HH BOT* 〙
+`
+}
+exports.iklan = iklan
