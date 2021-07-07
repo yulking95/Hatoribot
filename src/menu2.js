02f4d4d641b782a718d670977dc33343000b73fb
yulking95 <83798245+yulking95@users.noreply.github.com>
2021-07-07 13:40:42 -0500

    Create menu2.js

---
+++ src/menu2.js
@@ -0,0 +1,38 @@
+const menu2 = (prefix) => { 
+	return `                 
+╠══✪〘 MENU 〙✪══
+║
+╠➥ *${prefix}toimg*
+╠➥ *${prefix}tts*
+╠➥ *
+╠➥ *${prefix}leens [con palabra]*
+╠➥ *
+╠➥ *${prefix}setprefijo*
+║
+╠══✪〘 IMAGENS 〙✪══
+║
+╠➸ *
+╠➸ *${prefix}dueño*
+╠➸ *${prefix}porno*
+╠➸ *${prefix}buenasnoches*
+╠➸ *${prefix}buendia*
+╠➸ *${prefix}buenastardes*
+╠➸ *${prefix}mia*
+╠➸ *${prefix}mia1*
+╠➸ *${prefix}mia2*
+╠➸ *${prefix}belle*
+╠➸ *${prefix}belle1*
+╠➸ *${prefix}belle2*
+╠➸ *${prefix}belle3*
+╠➸ *${prefix}akeno*  
+╠➸ *
+║
+╠══✪〘 INTELIGÊNCIA IA 〙✪══
+║
+╠➸ *${prefix}simih 1 (para activar)*
+╠➸ *${prefix}simih 0 (para desactivar)*
+╠➥ *${prefix}simi (su mensaje)*
+║
+╠══✪〘 *HH BOT* 〙✪══`
+}
+exports.menu2 = menu2
